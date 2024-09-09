e55bdf619772954e459847b5aaead3c73bdbb065
Kurniaseptiadi153 <108206481+Kurniaseptiadi153@users.noreply.github.com>
2024-09-09 11:56:04 +0700

    Add files via upload

---
+++ TUGAS PROFILE KURNIA.php
@@ -0,0 +1,17 @@
+<?php
+// Menampilkan nama
+$nama = "Kurnia Septiadi";
+// Menampilkan umur
+$umur = 16;
+// Menampilkan sekolah
+$sekolah = "SMKN 2";
+// Menampilkan cita-cita
+$cita_cita = "ingin menjadi pengusaha bakwan";
+
+// Menampilkan informasi profile
+echo "<h1>Profile</h1>";
+echo "Nama: $nama<br>";
+echo "Umur: $umur tahun<br>";
+echo "Sekolah: $sekolah<br>";
+echo "Cita-cita: $cita_cita<br>";
+?>
